e69316d5e5953c6e2e069ab343da9b9ef5ac58b0
BereketZz <bereketzewde989@gmail.com>
2024-04-10 21:40:25 +0300

    school context done

--- frontend/app/context/toget.tsx
+++ frontend/app/context/toget.tsx
@@ -7,6 +7,7 @@ export const ToGetContext = createContext({});
 export function AppGetter({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const [dork, setDork] = useState("Goood");
+  const[schoolId, setSchoolId]= useState("Your school id goes here");
 
   return (
     <ToGetContext.Provider value={{ dork, setDork }}>
