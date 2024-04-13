e109b931dc3e81103c04d4bebde9e25c9a10aa2c
BereketZz <bereketzewde989@gmail.com>
2024-04-13 09:49:06 +0300

    merged

--- frontend/app/dashboard/grades/page.tsx
+++ frontend/app/dashboard/grades/page.tsx
@@ -1,11 +1,15 @@
-"use client"
-// Client-side code (React component)
-
-import React, { useEffect, useState } from 'react';
-import Main from '../main/Main';
-import axios from 'axios';
-import { AppContext } from '@/components/context/UserContext';
-import { ToastContainer } from 'react-toastify';
+"use client";
+import React, { useEffect, useState } from "react";
+import Main from "../main/Main";
+import axios from "axios";
+import { AppContext } from "@/components/context/UserContext";
+import { SchoolSharp } from "@mui/icons-material";
+import * as yup from "yup";
+import { useFormik } from "formik";
+// import DisplayTable from "./displayTable";
+import NewWay from "./newWay";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Page = () => {
 
