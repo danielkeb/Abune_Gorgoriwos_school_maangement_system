7b0e363f66c2f304fd6b370250f3cc74bc672b0f
Daniel <daniel@kali.kali>
2024-05-23 01:41:03 -0400

    completer

--- frontend/app/dashboard/teachers/manage.tsx
+++ frontend/app/dashboard/teachers/manage.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Box, Card, Typography, gridClasses } from "@mui/material";
+import { Box, Card, Typography, Grid } from "@mui/material";
 import React, { Fragment, useEffect, useMemo, useState } from "react";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import axios from "axios";
 import { Dialog, Transition } from "@headlessui/react";
 import UsersActions from "./UsersActions";
@@ -9,18 +9,53 @@ import { ToastContainer } from "react-toastify";
 import { grey } from "@mui/material/colors";
 import EditIcon from "@mui/icons-material/Edit";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import VisibilityIcon from "@mui/icons-material/Visibility";
 import { useRouter } from "next/navigation";
+
+interface User {
+  id: number;
+  image: string;
+  first_name: string;
+  middle_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  education_level: string;
+  grade: string[];
+  section: { id: number, name: string }[][];
+  subject: { id: number, name: string }[][];
+}
+
+interface Teacher {
+  id: number;
+  user: User;
+}
+
 function Manage() {
-  const [teachers, setTeachers] = useState([]);
+  const [teachers, setTeachers] = useState<Teacher[]>([]);
   const [pageSize, setPageSize] = useState(5);
-  const [rowId, setRowId] = useState(null);
+  const [rowId, setRowId] = useState<number | null>(null);
   const router = useRouter();
+  const [imageUrl, setImageUrl] = useState<string>('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("http://localhost:3333/teachers/get");
+        const res = await axios.get<Teacher[]>("http://localhost:3333/teachers/get");
         setTeachers(res.data);
+        console.log(res.data[0].image);
+        if (res.data.length > 0) {
+
+          const responseImg = await axios.get(`http://localhost:3333/uploads/${res.data[0].image}`, { responseType: 'blob' });
+
+          const url = URL.createObjectURL(responseImg.data);
+
+          setImageUrl(url);
+
+          console.log("url data", url)
+
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -28,46 +63,43 @@ function Manage() {
 
     fetchData();
   }, []);
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedUser, setSelectedUser] = useState(null);
 
-  function openDialog(user) {
+  function openDialog(user: User) {
     setSelectedUser(user);
-    
     setIsOpen(true);
   }
-
-  const filteredResult = teachers.filter(teacher => teacher.id === selectedUser);
-  console.log(filteredResult[0]?.frist_name)
 
   function closeDialog() {
     setIsOpen(false);
   }
-  function sendTo(id) {
-    router.push(
-    `/dashboard/teachup/${id}`,
 
-    );
+  function sendTo(id: number) {
+    router.push(`/dashboard/teachup/${id}`);
   }
- 
 
-  const columns = useMemo(
+  const columns: GridColDef[] = useMemo(
     () => [
-      { field: "id", headerName: "ID", width: "60" },
+      { field: "id", headerName: "ID", width: 60 },
+      {
+        field: "image",
+        headerName: "Photo",
+        width: 200,
+        renderCell: (params) => <img src={params.image} />
+        
+        
+      },
       {
         field: "frist_name",
         headerName: "First Name",
-        width: "200",
+        width: 200,
         type: "string",
         editable: true,
       },
-      { field: "middle_name", headerName: "Middle Name", width: "200" },
-      { field: "last_name", headerName: "Last Name", width: "200" },
-      { field: "email", headerName: "Email", width: "200" },
-      { field: "phone", headerName: "phone", width: "200" },
-      { field: "education_level", headerName: "Education Leve", width: "200" },
-      
-
+      { field: "middle_name", headerName: "Middle Name", width: 200 },
+      { field: "last_name", headerName: "Last Name", width: 200 },
+      { field: "email", headerName: "Email", width: 200 },
+      { field: "phone", headerName: "Phone", width: 200 },
+      { field: "education_level", headerName: "Education Level", width: 200 },
       {
         field: "edit",
         headerName: "Edit",
@@ -75,19 +107,21 @@ function Manage() {
         renderCell: (params) => (
           <button
             className="border-none bg-transparent w-full h-full"
-            onClick={() => sendTo(params.id)}>
+            onClick={() => sendTo(params.id)}
+          >
             <EditIcon />
           </button>
         ),
       },
     ],
-
     [rowId]
   );
 
+  const filteredResult = selectedUser ? teachers.filter(teacher => teacher.id === selectedUser.id) : [];
+
   return (
-    <div className="w-full mt-6 ml-4 flex justify-center items-center ">
-      <div className=" w-[80%]  bg-white boxshadow p-6">
+    <div className="w-full mt-6 ml-4 flex justify-center items-center">
+      <div className="w-[80%] bg-white boxshadow p-6">
         <DataGrid
           rows={teachers}
           getRowId={(row) => row.id}
@@ -101,23 +135,23 @@ function Manage() {
             bottom: params.isLastVisible ? 0 : 5,
           })}
           sx={{
-            [`& .${gridClasses.row}`]: {
+            [`& .${Grid.row}`]: {
               bgcolor: (theme) =>
-                theme.palette.mode == "light" ? grey[200] : grey[900],
+                theme.palette.mode === "light" ? grey[200] : grey[900],
             },
           }}
           slots={{ toolbar: GridToolbar }}
           onCellKeyDown={(params) => setRowId(params.id)}
-          onCellEditStart={(params) => setRowId(params.id)}>
-          {" "}
-        </DataGrid>
+          onCellEditStart={(params) => setRowId(params.id)}
+        />
       </div>
       <Transition.Root show={isOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="fixed z-10 inset-0 overflow-y-auto "
-          onClose={closeDialog}>
-          <div className="flex items-center justify-center min-h-screen ">
+          className="fixed z-10 inset-0 overflow-y-auto"
+          onClose={closeDialog}
+        >
+          <div className="flex items-center justify-center min-h-screen">
             <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
 
             <Transition.Child
@@ -127,61 +161,66 @@ function Manage() {
               enterTo="opacity-100 scale-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95">
-              <div className="bg-white rounded-lg p-6 w-[500px] mx-auto z-10 ">
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="bg-white rounded-lg p-6 w-[500px] mx-auto z-10">
                 <Dialog.Title className="text-lg font-semibold mb-2">
                   User Information
                 </Dialog.Title>
                 {selectedUser && (
-                  <div className="flex   space-x-4">
+                  <div className="flex space-x-4">
                     <div>
                       <AccountCircleIcon sx={{ fontSize: "60px" }} />
                     </div>
                     <div>
                       <p className="text-xl font-semibold">
-                        {filteredResult[0]?.frist_name} {filteredResult[0]?.last_name}
+                        {filteredResult[0]?.user.first_name} {filteredResult[0]?.user.last_name}
                       </p>
-
-                      <p className="text-gray-500">{filteredResult[0]?.email}</p>
-                      <div className="flex gap-4  ">
-                        <div className="mt-4 ">
+                      <p className="text-gray-500">{filteredResult[0]?.user.email}</p>
+                      <div className="flex gap-4">
+                        <div className="mt-4">
                           <label
                             htmlFor="grade"
-                            className="block font-medium text-gray-700">
+                            className="block font-medium text-gray-700"
+                          >
                             Allocated Grade
                           </label>
                           <select
                             id="grade"
                             name="grade"
                             className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
-                            value={filteredResult[0]?.grade[0]} // Assuming the first grade is selected
+                            value={filteredResult[0]?.user.grade[0]}
                           >
-                            {filteredResult[0]?.grade.map((grade, index) => (
+                            {filteredResult[0]?.user.grade.map((grade, index) => (
                               <option key={index} value={grade}>
                                 {grade}
                               </option>
                             ))}
                           </select>
                         </div>
-                        <div className="mt-4 ">
+                        <div className="mt-4">
                           <label
                             htmlFor="section"
-                            className="block font-medium text-gray-700">
+                            className="block font-medium text-gray-700"
+                          >
                             Allocated Sections
                           </label>
                           <select
                             id="section"
                             name="section"
-                            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm">
-                            {filteredResult[0]?.section.map(
+                            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                          >
+                            {filteredResult[0]?.user.section.map(
                               (sectionArray, gradeIndex) => (
                                 <optgroup
                                   key={gradeIndex}
-                                  label={`Grade ${filteredResult[0]?.grade[gradeIndex]}`}>
+                                  label={`Grade ${filteredResult[0]?.user.grade[gradeIndex]}`}
+                                >
                                   {sectionArray.map((section, index) => (
                                     <option
                                       key={`${gradeIndex}-${index}`}
-                                      value={section.id}>
+                                      value={section.id}
+                                    >
                                       {section.name}
                                     </option>
                                   ))}
@@ -191,25 +230,29 @@ function Manage() {
                           </select>
                         </div>
                       </div>
-                      <div className="mt-4 ">
+                      <div className="mt-4">
                         <label
                           htmlFor="subjects"
-                          className="block font-medium text-gray-700">
+                          className="block font-medium text-gray-700"
+                        >
                           Allocated Subjects
                         </label>
                         <select
                           id="subjects"
                           name="subjects"
-                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm">
-                          {filteredResult[0]?.subject.map(
+                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                        >
+                          {filteredResult[0]?.user.subject.map(
                             (subjectArray, gradeIndex) => (
                               <optgroup
                                 key={gradeIndex}
-                                label={`Grade ${filteredResult[0]?.grade[gradeIndex]}`}>
+                                label={`Grade ${filteredResult[0]?.user.grade[gradeIndex]}`}
+                              >
                                 {subjectArray.map((subject, index) => (
                                   <option
                                     key={`${gradeIndex}-${index}`}
-                                    value={subject.id}>
+                                    value={subject.id}
+                                  >
                                     {subject.name}
                                   </option>
                                 ))}
@@ -223,7 +266,8 @@ function Manage() {
                 )}
                 <button
                   onClick={closeDialog}
-                  className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded mt-4">
+                  className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded mt-4"
+                >
                   Close
                 </button>
               </div>
