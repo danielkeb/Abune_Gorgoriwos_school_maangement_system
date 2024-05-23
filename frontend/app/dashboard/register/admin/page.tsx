395778a4b4cded00fa10db928d3cd8a51016780e
BereketZz <bereketzewde989@gmail.com>
2024-05-23 09:50:02 +0300

    admin page fixed

--- frontend/app/dashboard/register/admin/page.tsx
+++ frontend/app/dashboard/register/admin/page.tsx
@@ -46,7 +46,7 @@ const page = () => {
       try {
         setCheck(true);
         const formData = new FormData();
-        Object.keys(values.image).forEach(key => {
+        Object.keys(values.image).forEach((key) => {
           formData.append(key, values.image as any);
         });
         const response = await axios.post(
@@ -63,7 +63,7 @@ const page = () => {
 
         toast.success("Admin Registerd ");
         // router.push('/head')
-      } catch (error:any) {
+      } catch (error: any) {
         toast.error(error?.response?.data.message);
       }
       setCheck(false);
@@ -182,8 +182,8 @@ const page = () => {
                       <option value="" disabled>
                         Select an option
                       </option>
-                      <option value="option1">Male</option>
-                      <option value="option2">Female</option>
+                      <option value="Male">Male</option>
+                      <option value="Female">Female</option>
                     </select>
                   </div>
                 </div>
@@ -277,23 +277,25 @@ const page = () => {
                       Select school
                     </label>
                     <select
-  id="yourSelect"
-  name="school_name" // Changed from gender to school_id
-  value={formik.values.school_name} // Changed from school_name to school_id
-  onChange={formik.handleChange}
-  onBlur={formik.handleBlur}
-  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none w-full focus:border-2 focus:border-gray-400"
->
-  <option value="">
-    Select a school
-  </option>
-  {schoolss.map((school: { school_name: string; id: number; school_address: string }) => (
-    <option key={school.id} value={school.id}>
-      {school.school_name}
-    </option>
-  ))}
-</select>
-
+                      id="yourSelect"
+                      name="school_name" // Changed from gender to school_id
+                      value={formik.values.school_name} // Changed from school_name to school_id
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none w-full focus:border-2 focus:border-gray-400">
+                      <option value="">Select a school</option>
+                      {schoolss.map(
+                        (school: {
+                          school_name: string;
+                          id: number;
+                          school_address: string;
+                        }) => (
+                          <option key={school.id} value={school.id}>
+                            {school.school_name}
+                          </option>
+                        )
+                      )}
+                    </select>
                   </div>
                 </div>
               </div>
@@ -314,7 +316,9 @@ const page = () => {
                       className="w-full bg-white p-5 flex justify-center items-center"
                       htmlFor="photo">
                       {selectedFileName ? (
-                        <span className="text-green-700">{selectedFileName}</span>
+                        <span className="text-green-700">
+                          {selectedFileName}
+                        </span>
                       ) : (
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -345,20 +349,32 @@ const page = () => {
                 </div>
               </div>
               <br />
-              {/* <button className="bg-green-700  border-0 text-white w-full p-3  rounded-md">
+              <button type="submit" className="bg-green-700  border-0 text-white w-full p-3  rounded-md">
               { check? <>
               
               <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
           <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
          </svg>  Submitting  ...</>:"Submit"}
-              </button> */}
-               <button
+              </button>
+              {/* <button
                 type="submit"
                 className="bg-green-700  border-0 text-white w-full p-3  rounded-md">
                 Submit
-              </button>
+              </button> */}
             </form>
+            <ToastContainer
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
           </div>
         </RegistrationHead>
       </Main>
